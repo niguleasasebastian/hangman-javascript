6c75496ce4477d15697a122035820bca235449ad
niguleasasebastian <niguleasasebastian@yahoo.com>
2021-03-26 17:28:50 +0200

    added medias and loosing game message

--- hangman using api/app.js
+++ hangman using api/app.js
@@ -2,6 +2,9 @@
 // API GET WORD
 // API GET WORD
 const apiUrl = "https://random-words-api.vercel.app/word";
+const secretWordDiv = document.querySelector(".secret-word");
+let isRunning = true;
+
 async function getWordFromApi() {
   const params = {
     method: "GET",
@@ -24,7 +27,6 @@ async function getWordFromApi() {
 
   return word[0].word;
 }
-const secretWordDiv = document.querySelector(".secret-word");
 getWordFromApi().then(async function (word) {
   let secretWord = "";
   for (let i = 0; i < word.length; i++) {
@@ -38,30 +40,40 @@ getWordFromApi().then(async function (word) {
   let contor = 1;
   const buttons = document.querySelector(".buttons");
   buttons.addEventListener("click", (e) => {
-    let letter;
-    if (e.target.classList.contains("button-letter")) {
-      letter = e.target.innerHTML.toLowerCase();
-      let vector = checkLetter(word.toLowerCase(), letter);
-      if (vector.length != 0) {
-        let aux = 0;
-        for (let i = 0; i < word.length; i++) {
-          if (i == vector[aux]) {
-            secretWord =
-              secretWord.substring(0, i) + letter + secretWord.substring(i + 1);
-            aux++;
-            console.log(secretWord);
+    if (isRunning == true) {
+      let letter;
+      if (e.target.classList.contains("button-letter")) {
+        letter = e.target.innerHTML.toLowerCase();
+        let vector = checkLetter(word.toLowerCase(), letter);
+        if (vector.length != 0) {
+          let aux = 0;
+          for (let i = 0; i < word.length; i++) {
+            if (i == vector[aux]) {
+              secretWord =
+                secretWord.substring(0, i) +
+                letter +
+                secretWord.substring(i + 1);
+              aux++;
+              console.log(secretWord);
+            }
+          }
+          secretWordDiv.innerHTML = "";
+          for (let i = 0; i < secretWord.length; i++)
+            secretWordDiv.innerHTML += secretWord[i] + " ";
+        } else {
+          contor++;
+          const image = document.querySelector(".hangman-image");
+          if (contor < 8) {
+            image.src = `art/${contor}.png`;
+          } else {
+            image.src = `art/${contor}.png`;
+            isRunning = false;
+            stopGame(word);
           }
         }
-      } else {
-        contor++;
-        const image = document.querySelector(".hangman-image");
-        image.src = `art/${contor}.png`;
-      }
-      secretWordDiv.innerHTML = "";
-      for (let i = 0; i < secretWord.length; i++)
-        secretWordDiv.innerHTML += secretWord[i] + " ";
 
-      e.target.style.display = "none";
+        e.target.style.display = "none";
+      }
     }
   });
 });
@@ -70,4 +82,9 @@ function checkLetter(word, letter) {
   let vector = [];
   for (let i = 0; i < word.length; i++) if (word[i] == letter) vector.push(i);
   return vector;
+}
+
+function stopGame(word) {
+  console.log("Game stopped");
+  secretWordDiv.innerText = `You lost, the word was ${word}`;
 }
